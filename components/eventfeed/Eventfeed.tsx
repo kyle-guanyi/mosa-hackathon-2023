decdafb146904e19c1f7e2d947b3c4f062a7cccc
kebinjpeg <kevinocca@gmail.com>
2023-08-02 05:43:19 -0700

    creating eventfeed

---
+++ components/eventfeed/Eventfeed.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./Card";
+
+const Eventfeed = () => {
+    return (
+        <section
+        id ="eventfeed"
+        className="w-full py-20 border-l-1 border-r-1 border-t-1 border-gray-600">
+            
+        </section>
+    )
+}
