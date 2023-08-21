e73954652eea2f82482fbe5476c310a6bd6d266b
Bonnie Tse <bonniet@seas.upenn.edu>
2023-08-20 22:59:17 -0700

    add filter for eventfeed with dropdown for cities and calendar for date

--- components/eventfeed/Eventfeed.tsx
+++ components/eventfeed/Eventfeed.tsx
@@ -1,47 +1,127 @@
-//turn event feed into a client component
 'use client';
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
-import { useState, useEffect } from 'react';
 
 const EventCardList = ({data}) => {
     return (
-        <div className = "mt-16 prompt_layout">
+        <div className="mt-16 prompt_layout">
             {data.map((event) => (
                 <Card
-                key={event._id}
-                event = {event}
+                    key={event._id}
+                    event={event}
                 />
             ))}
-        </div> 
-    )
+        </div>
+    );
 }
 
 const Eventfeed = () => {
-
-    const[events, setEvents] = useState([]);
+    const [events, setEvents] = useState([]);
+    const [filteredEvents, setFilteredEvents] = useState([]);
+    const [filterCity, setFilterCity] = useState('');
+    const [filterDate, setFilterDate] = useState('');
 
     const fetchEvents = async () => {
         const response = await fetch('/api/event');
         const data = await response.json();
-        setEvents(data); 
+        setEvents(data);
     }
-    // fetches data from database and set it to empty array 'events'
+
     useEffect(() => {
         fetchEvents();
     }, []);
-    
+
+    useEffect(() => {
+        const results = events.filter(event =>
+            event.closestCity.includes(filterCity) &&
+            (!filterDate || new Date(event.startDate).toDateString() === new Date(filterDate).toDateString())
+        );
+        setFilteredEvents(results);
+    }, [events, filterCity, filterDate]);
+
+        return (
+            <section id="eventfeed" className="w-full border-l-1 border-r-1 border-t-1 border-gray-600">
+                <div className="p-4 flex space-x-4">
+                    <select
+                        value={filterCity}
+                        onChange={(e) => setFilterCity(e.target.value)}
+                        className="block appearance-none w-1/4 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                    >
+                        <option value="">Select City</option>
+                        <option value="Virtual Event">Virtual Event</option>
+                        <option value="Africa">Africa</option>
+                        <option value="Atlanta">Atlanta</option>
+                        <option value="Austin">Austin</option>
+                        <option value="Australia">Australia</option>
+                        <option value="Bay Area">Bay Area</option>
+                        <option value="Berlin">Berlin</option>
+                        <option value="Boston">Boston</option>
+                        <option value="CDMX">CDMX</option>
+                        <option value="Chicago">Chicago</option>
+                        <option value="China">China</option>
+                        <option value="Colorado">Colorado</option>
+                        <option value="DC">DC</option>
+                        <option value="DFW">DFW</option>
+                        <option value="Europe">Europe</option>
+                        <option value="Florida">Florida</option>
+                        <option value="Hong Kong">Hong Kong</option>
+                        <option value="Houston">Houston</option>
+                        <option value="India">India</option>
+                        <option value="Indonesia">Indonesia</option>
+                        <option value="Japan">Japan</option>
+                        <option value="Korea">Korea</option>
+                        <option value="Latin America">Latin America</option>
+                        <option value="London">London</option>
+                        <option value="Los Angeles">Los Angeles</option>
+                        <option value="Midwest">Midwest</option>
+                        <option value="Montreal">Montreal</option>
+                        <option value="Nankai">Nankai</option>
+                        <option value="New Jersey">New Jersey</option>
+                        <option value="New Zealand">New Zealand</option>
+                        <option value="NYC">NYC</option>
+                        <option value="Philippines">Philippines</option>
+                        <option value="Philly">Philly</option>
+                        <option value="Portland PDX">Portland PDX</option>
+                        <option value="Salt Lake City">Salt Lake City</option>
+                        <option value="Seattle">Seattle</option>
+                        <option value="Singapore">Singapore</option>
+                        <option value="Toronto">Toronto</option>
+                        <option value="Vancouver">Vancouver</option>
+                        <option value="Vietnam">Vietnam</option>
+                    </select>
+                    <input
+                        type="date"
+                        value={filterDate}
+                        onChange={(e) => setFilterDate(e.target.value)}
+                        className="appearance-none block w-1/4 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                    />
+                </div>
+                <EventCardList data={filteredEvents} />
+            </section>
+        );
+    }
+
+    export default Eventfeed;
 
 
-    return (
-        <section
-        id ="eventfeed"
-        className="w-full border-l-1 border-r-1 border-t-1 border-gray-600">
-            <EventCardList
-                data={events}
-            />
-        </section>
-    )
-}
+//     return (
+//         <section id="eventfeed" className="w-full border-l-1 border-r-1 border-t-1 border-gray-600">
+//             <div className="p-4">
+//                 <input
+//                     type="text"
+//                     placeholder="Filter by city"
+//                     value={filterCity}
+//                     onChange={(e) => setFilterCity(e.target.value)}
+//                 />
+//                 <input
+//                     type="date"
+//                     value={filterDate}
+//                     onChange={(e) => setFilterDate(e.target.value)}
+//                 />
+//             </div>
+//             <EventCardList data={filteredEvents} />
+//         </section>
+//     );
+// }
 
-export default Eventfeed;
+// export default Eventfeed;
