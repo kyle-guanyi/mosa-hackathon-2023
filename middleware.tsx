6a269f758bb431780e956591d8bf694e7412d3a5
Bonnie Tse <bonniet@seas.upenn.edu>
2023-08-27 07:38:00 -0700

    added comments to middleware

--- middleware.tsx
+++ middleware.tsx
@@ -1,20 +1,25 @@
 import { withAuth } from "next-auth/middleware";
 
+/**
+ * This is a middleware that will redirect the user to the sign in page if user is not signed in
+ */
 export default withAuth({
   pages: {
     signIn: "/",
   },
 });
 
+/**
+ * This is a middleware that matches all routes except for the ones starting with:
+ *
+ * - api (API routes)
+ * - _next/static (static files)
+ * - _next/image (image optimization files)
+ * - favicon.ico (favicon file)
+ *
+ */
 export const config = {
     matcher: [
-      /*
-       * Match all request paths except for the ones starting with:
-       * - api (API routes)
-       * - _next/static (static files)
-       * - _next/image (image optimization files)
-       * - favicon.ico (favicon file)
-       */
       '/((?!about).*)',
     ],
   }
