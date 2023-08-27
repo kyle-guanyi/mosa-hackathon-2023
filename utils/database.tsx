8fbc5d3e185d3699a0c41d8b1764cba8f492831e
Bonnie Tse <bonniet@seas.upenn.edu>
2023-08-27 07:34:19 -0700

    added comments to util folder database

--- utils/database.tsx
+++ utils/database.tsx
@@ -2,14 +2,19 @@ import mongoose from 'mongoose';
 
 let isConnected = false; // track the connection
 
+/**
+ * This function connects to our MongoDB database, and handles any bad connections
+ */
 export const connectToDB = async () => {
   mongoose.set('strictQuery', true);
 
+  // check if we have a connection to the database or if it's currently
   if(isConnected) {
     console.log('MongoDB is already connected');
     return;
   }
 
+  // connect to our MongoDB database, and handle an bad connections
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "share_event",
