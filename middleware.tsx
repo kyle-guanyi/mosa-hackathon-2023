b7ba6f5aafb76b30eeb6ca31bf1acb90b21a9de9
kyle-guanyi <kyle.guanyi.li@gmail.com>
2023-09-22 18:27:09 -0700

    fix for unauthorized users redirecting to correct url after signing in

--- middleware.tsx
+++ middleware.tsx
@@ -9,6 +9,14 @@ export default withAuth({
     signIn: "/",
     error: "/",
   },
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url
+      return baseUrl
+    }
 });
 
 /**
